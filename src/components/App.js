a57e91ec3e485a47410093457ceccb9b8ea89b31
Shubham Bhardwaj <31105326+ShubhamB99@users.noreply.github.com>
2020-07-09 12:09:03 +0530

    Updating changes for v2

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
 import './App.css';
-// import Meme from '../abis/Meme.json'
-import Ethreon from '../abis/Ethreon.json'
+import Ethreon from '../abis/Ethreon.json';
 
 const ipfsClient = require('ipfs-http-client')
 const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
@@ -32,21 +31,42 @@ class App extends Component {
     // Load account
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
+    this.setState({ web3 })
     const networkId = await web3.eth.net.getId()
     const networkData = Ethreon.networks[networkId]
     if(networkData) {
+      // console.log(networkData)
       const contract = web3.eth.Contract(Ethreon.abi, networkData.address)
       this.setState({ contract })
+      // console.log(this.state.contract);
+      this.getCreatorElseSignup();
+
       // const imgHash = await contract.methods.ge().call()
-      const imgHash = "QmeaKRPVpAndtLYRZVWcWG34proxjr9Z9wiNtaseSKfsTZ"
-      this.setState({ imgHash })
+      // const imgHash = "QmeaKRPVpAndtLYRZVWcWG34proxjr9Z9wiNtaseSKfsTZ"
+      // this.setState({ imgHash })
     } else {
       window.alert('Smart contract not deployed to detected network.')
     }
-    console.log(this.state.contract);
-    this.state.contract.methods.newCreatorSignup().send({ from: this.state.account }).then(function(result) {
-      console.log(result);
-    })
+    // console.log('Starting print!')
+    // console.log(this.state.imgHash)
+    // console.log(this.state.contentHash)
+    console.log(this.state.contract)
+    // console.log(this.state.web3)
+    // console.log(this.state.buffer)
+    // console.log(this.state.account)
+  }
+
+  async getCreatorElseSignup() {
+    const isOldCreator = await this.state.contract.methods.isOldCreator().call({from: this.state.account});
+    console.log('isOldCreator', isOldCreator);
+    if (isOldCreator == false) {
+      this.state.contract.methods.newCreatorSignup().send({ from: this.state.account }).then(function(result) {
+        console.log(result);
+      })
+    }
+    else {
+      console.log('Welcome Back Creator!')
+    }
   }
 
   constructor(props) {
@@ -73,22 +93,7 @@ class App extends Component {
     }
   }
 
-  onSubmit = (event) => {
-    event.preventDefault()
-    console.log("Submitting file to ipfs...")
-    ipfs.add(this.state.buffer, (error, result) => {
-      console.log('Ipfs result', result)
-      if(error) {
-        console.error(error)
-        return
-      }      
-       this.state.contract.methods.addCreatorImage(result[0].hash).send({ from: this.state.account }).then((r) => {
-         return this.setState({ imgHash: result[0].hash })
-       })
-    })
-  }
-
-  onSubmit2 = (event) => {
+  onSubmitImage = (event) => {
     event.preventDefault()
     console.log("Submitting file to ipfs...")
     ipfs.add(this.state.buffer, (error, result) => {
@@ -97,21 +102,34 @@ class App extends Component {
         console.error(error)
         return
       }
-       this.state.contract.methods.addCreatorContent(result[0].hash).send({ from: this.state.account }).then((r) => {
-         return this.setState({ contentHash: result[0].hash })
-       })
+      this.state.contract.methods.addCreatorImage(result[0].hash).send({ from: this.state.account }).then((r) => {
+        console.log('Result', r)
+        console.log('Image hash ', result[0].hash)
+        return this.setState({ imgHash: result[0].hash })
+      })
+    })
+  }
+
+  onSubmitContent = (event) => {
+    event.preventDefault()
+    console.log("Submitting file to ipfs...")
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('Ipfs result', result)
+      if(error) {
+        console.error(error)
+        return
+      }
+      this.state.contract.methods.addCreatorContent(result[0].hash).send({ from: this.state.account }).then((r) => {
+        console.log('Result', r)
+        console.log('Content hash ', result[0].hash)
+        return this.setState({ contentHash: result[0].hash })
+      })
     })
   }
 
   render() {
     return (
       <div>
-        {/* <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0" rel="noopener noreferrer">
-            Ethreon - The Creator's Platform
-          </a>
-        </nav> */}
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -121,13 +139,13 @@ class App extends Component {
                 </a>
                 <p>&nbsp;</p>
                 <h2>Change Image</h2>
-                <form onSubmit={this.onSubmit} >
+                <form onSubmit={this.onSubmitImage} >
                   <input type='file' onChange={this.captureFile} />
                   <input type='submit' />
                 </form>
                 <p>&nbsp;</p>
                 <h2>Change Content</h2>
-                <form onSubmit={this.onSubmit2} >
+                <form onSubmit={this.onSubmitContent} >
                   <input type='file' onChange={this.captureFile} />
                   <input type='submit' />
                 </form>
